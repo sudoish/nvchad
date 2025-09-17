8f93d022ccd22d102c335ff89f28342c6ac24cfc
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2025-09-17 15:06:36 -0400

    reformat and disable tabs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,55 +1,56 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
-	transparency = true,
+  theme = "catppuccin",
+  transparency = true,
 
-	-- Force transparency for common groups still setting bg
-	hl_override = {
-		Normal          = { bg = "NONE" },
-		NormalNC        = { bg = "NONE" },
-		NormalFloat     = { bg = "NONE" },
-		FloatBorder     = { bg = "NONE" },
-		SignColumn      = { bg = "NONE" },
-		LineNr          = { bg = "NONE" },
-		CursorLine      = { bg = "NONE" },
-		CursorLineNr    = { bg = "NONE" },
-		Folded          = { bg = "NONE" },
-		EndOfBuffer     = { bg = "NONE" },
-		WinSeparator    = { bg = "NONE" },
-		VertSplit       = { bg = "NONE" },
+  -- Force transparency for common groups still setting bg
+  hl_override = {
+    Normal = { bg = "NONE" },
+    NormalNC = { bg = "NONE" },
+    NormalFloat = { bg = "NONE" },
+    FloatBorder = { bg = "NONE" },
+    SignColumn = { bg = "NONE" },
+    LineNr = { bg = "NONE" },
+    CursorLine = { bg = "NONE" },
+    CursorLineNr = { bg = "NONE" },
+    Folded = { bg = "NONE" },
+    EndOfBuffer = { bg = "NONE" },
+    WinSeparator = { bg = "NONE" },
+    VertSplit = { bg = "NONE" },
 
-		-- statusline/tabline
-		StatusLine      = { bg = "NONE" },
-		StatusLineNC    = { bg = "NONE" },
-		TabLine         = { bg = "NONE" },
-		TabLineFill     = { bg = "NONE" },
-		TabLineSel      = { bg = "NONE" },
+    -- statusline/tabline
+    StatusLine = { bg = "NONE" },
+    StatusLineNC = { bg = "NONE" },
+    TabLine = { bg = "NONE" },
+    TabLineFill = { bg = "NONE" },
+    TabLineSel = { bg = "NONE" },
 
-		-- popup/menu
-		Pmenu           = { bg = "NONE" },
-		PmenuSel        = { bg = "NONE" },
+    -- popup/menu
+    Pmenu = { bg = "NONE" },
+    PmenuSel = { bg = "NONE" },
 
-		-- Telescope
-		TelescopeNormal = { bg = "NONE" },
-		TelescopeBorder = { bg = "NONE" },
+    -- Telescope
+    TelescopeNormal = { bg = "NONE" },
+    TelescopeBorder = { bg = "NONE" },
 
-		-- Trees/sidebars (NvChad often uses nvim-tree)
-		NvimTreeNormal       = { bg = "NONE" },
-		NvimTreeEndOfBuffer  = { bg = "NONE" },
-	},
+    -- Trees/sidebars (NvChad often uses nvim-tree)
+    NvimTreeNormal = { bg = "NONE" },
+    NvimTreeEndOfBuffer = { bg = "NONE" },
+  },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = { load_on_startup = true }
+M.ui = {
+  tabufline = {
+    enable = false,
+    lazyload = false,
+  },
+}
 
 return M
