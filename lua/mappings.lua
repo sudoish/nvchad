4a1407896c67e297d6478c65ad34a42383f7dbfc
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2026-01-04 21:13:22 -0500

    Add support to navigate between tmux windows

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,45 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Smart navigation: Vim splits -> tmux panes -> tmux windows
+local function navigate(direction)
+  print("navigate called: " .. direction)
+
+  local vim_dir = ({ h = "h", j = "j", k = "k", l = "l" })[direction]
+  local tmux_pane_dir = ({ h = "L", j = "D", k = "U", l = "R" })[direction]
+  local tmux_edge_check = ({ h = "left", j = "bottom", k = "top", l = "right" })[direction]
+  local tmux_window_cmd = ({ h = "previous-window", l = "next-window" })[direction]
+
+  -- Try Vim split navigation first
+  local current_win = vim.fn.winnr()
+  vim.cmd("wincmd " .. vim_dir)
+  if vim.fn.winnr() ~= current_win then
+    print("moved to vim split")
+    return
+  end
+
+  -- Check if we're inside tmux
+  print("TMUX env: " .. tostring(vim.env.TMUX))
+  if not vim.env.TMUX then
+    print("not in tmux, returning")
+    return
+  end
+
+  -- Check if there's a tmux pane in that direction
+  local at_edge = vim.fn.trim(vim.fn.system("tmux display-message -p '#{pane_at_" .. tmux_edge_check .. "}'"))
+  print("at_edge=[" .. at_edge .. "] dir=" .. direction)
+  if at_edge == "0" then
+    vim.fn.system("tmux select-pane -" .. tmux_pane_dir)
+  elseif tmux_window_cmd then
+    vim.fn.jobstart({ "tmux", tmux_window_cmd }, { detach = true })
+  end
+end
+
+map("n", "<C-h>", function() navigate("h") end, { desc = "Navigate left" })
+map("n", "<C-l>", function() navigate("l") end, { desc = "Navigate right" })
+map("n", "<C-k>", function() navigate("k") end, { desc = "Navigate up" })
+map("n", "<C-j>", function() navigate("j") end, { desc = "Navigate down" })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
